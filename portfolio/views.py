23f4d4c74c4b5a2534e763270bcf6e19c69005dc
“SotoAkie” <112214as@gmail.com>
2025-06-12 12:05:02 +0900

    ポートフォリオページの見出しを作成した

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('ポートフォリオ')
+    return render(request,'portfolio.html')
 
-# Create your views here.
