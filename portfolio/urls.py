099fb34fb68ddac921eb7ef805751b3c8e041904
“SotoAkie” <112214as@gmail.com>
2025-06-14 17:13:37 +0900

    pdfのリンクの修正とアプリに遷移するボタンを作成

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'portfolio'
 
 urlpatterns = [
     path('', views.index, name='index'),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
