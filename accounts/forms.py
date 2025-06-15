996fc9806313893030a7b3a14750096a835ff39f
“SotoAkie” <112214as@gmail.com>
2025-06-15 20:23:48 +0900

    ユーザー登録画面のformを作成した

---
+++ accounts/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
+
+class SignupForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(
+        label='パスワード再入力', widget=forms.PasswordInput()
+    )
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        labels = {
+            'username': '名前',
+            'email': 'メールアドレス',
+            'password': 'パスワード',
+        }
+        widgets ={'password': forms.PasswordInput()}
+    
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password !=confirm_password:
+            self.add_error('confirm_password', 'パスワードが一致しません')
+        if password:
+            try:
+                validate_password(password, self.instance)
+            except ValidationError as e:
+                self.add_error('password', e)
+        return cleaned_data
+    
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .import forms
 
 def signup(request):
+    signup_form = forms.SignupForm(request.POST or None)
+    if signup_form.is_valid():
+        user = signup_form.save(commit=False)
+        user.set_password(signup_form.cleaned_data['password'])
+        user.save()
+        return redirect('accounts:home')
     return render(
-        request, 'accounts/signup.html'
+        request, 'accounts/signup.html',context= {
+            'signup_form': signup_form,
+            'hide_navbar': True
+        }
     )
