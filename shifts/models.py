cf66b19585bbaec1d7ed7f1b2c3f7193d78fde26
“SotoAkie” <112214as@gmail.com>
2025-06-24 23:38:41 +0900

    休み申請画面のmodelを作成した

--- shifts/models.py
+++ shifts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ShiftRequest(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user' , 'date')
+
+    def __str__(self):
+        return f"{self.user} requests off on {self.date}"
+
