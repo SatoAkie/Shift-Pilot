13c7cf7e941c42cfaa6c687245c8413744b63a67
“SotoAkie” <112214as@gmail.com>
2025-07-01 03:07:34 +0900

    エラー解決のためにマイグレーションのやり直し

--- shifts/models.py
+++ shifts/models.py
@@ -34,22 +34,27 @@ class ShiftPattern(models.Model):
 class Shift(models.Model):
     pattern = models.ForeignKey('ShiftPattern', on_delete=models.SET_NULL, null=True)
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
-
     date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = ('date', 'team', 'pattern')
+
+    def __str__(self):
+        return f"{self.date} - {self.team} - {self.pattern}"
+
 class UserShift(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    shift = models.ForeignKey(Shift,  on_delete=models.SET_NULL, null=True, blank=True)
-    shift_pattern = models.ForeignKey(ShiftPattern, on_delete=models.SET_NULL, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    shift = models.ForeignKey('Shift', on_delete=models.CASCADE)
+    shift_pattern = models.ForeignKey('ShiftPattern', null=True, blank=True, on_delete=models.SET_NULL)
+    is_manual = models.BooleanField(default=False)
 
     class Meta:
-            unique_together = ('user', 'shift')
+        unique_together = ('user', 'shift')
 
-
+    def __str__(self):
+        return f"{self.user} - {self.shift.date} - {self.shift_pattern or self.shift.pattern}"
     
 class PatternAssignmentSummary(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
